de437876bfb3f34e5ceb85291664579731282f6f
noufipalu <noufipalu786@gmail.com>
2023-03-27 22:09:39 +0530

    MobileAuthActivity

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -13,10 +14,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     EditText Email, Password, cPassword;
     Button register;
     FirebaseAuth mAuth;
+    FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         cPassword = findViewById(R.id.txt_conpass);
         register = findViewById(R.id.register);
         mAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +58,29 @@ public class MainActivity extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String Email_ = Email.getText().toString();
+                String Password_ = Password.getText().toString();
+
+                Map<String, Object> user = new HashMap<>();
+                user.put("Email", Email_);
+                user.put("Password", Password_);
+
+                firestore.collection("user").add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(MainActivity.this, "Data saved successfully", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, "Failed to save data", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
                 createUser();
             }
         });

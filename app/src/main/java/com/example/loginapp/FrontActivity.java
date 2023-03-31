9a6a151aca0994565884ae8f7781558f3848e43d
noufipalu <noufipalu786@gmail.com>
2023-03-31 11:12:16 +0530

    Show Logged in User

--- app/src/main/java/com/example/loginapp/FrontActivity.java
+++ app/src/main/java/com/example/loginapp/FrontActivity.java
@@ -1,14 +1,64 @@
 package com.example.loginapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Printer;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class FrontActivity extends AppCompatActivity {
 
+    TextView user;
+    FirebaseFirestore firestore;
+    FirebaseUser logUser;
+    String userId;
+    FirebaseAuth auth;
+    DocumentReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front);
+
+        user = findViewById(R.id.txt_user);
+        logUser = FirebaseAuth.getInstance().getCurrentUser();
+        userId = FirebaseAuth.getInstance().getUid();
+
+        firestore = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        reference = firestore.collection("user").document(userId);
+
+        reference.get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()){
+                            user.setText(logUser.getEmail());
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(FrontActivity.this, "Error: " + e, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/loginapp/LoginActivity.java
+++ app/src/main/java/com/example/loginapp/LoginActivity.java
@@ -16,6 +16,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,6 +25,9 @@ public class LoginActivity extends AppCompatActivity {
     TextView forgot;
     EditText Email, Password;
     FirebaseAuth mAuth;
+    FirebaseUser user;
+    String currentId;
+    FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +40,9 @@ public class LoginActivity extends AppCompatActivity {
         Email = findViewById(R.id.email_txt);
         Password = findViewById(R.id.pass_txt);
 
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        currentId = user.getUid();
+        firestore = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
 
         Register.setOnClickListener(new View.OnClickListener() {
@@ -75,7 +83,7 @@ public class LoginActivity extends AppCompatActivity {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()){
-                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(LoginActivity.this, "Logged In Successfully", Toast.LENGTH_SHORT).show();
                         Intent intent = new Intent(LoginActivity.this, FrontActivity.class);
                         intent.putExtra("Email", mAuth.getCurrentUser().getEmail());
                         startActivity(intent);
